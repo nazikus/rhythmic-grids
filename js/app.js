516dc114d5de176bfa787ff3dd68bbe850330d20
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2016-04-05 22:34:39 +0300

    adding updates to gh-pages

--- js/app.js
+++ js/app.js
@@ -1214,6 +1214,36 @@ function pauseEvent(e){
     e.returnValue=false;
     return false;
 }
+(function($) {
+	$(document).ready(function(){
+		var creditsToggleBtn = $('#credits-toggle'),
+		creditsSection = $('#credits-section'),
+		initialSection = $('#initial-section');
+
+		creditsToggleBtn.on('click', function(e) {
+			e.preventDefault();
+			var isCreditsActive = $(this).data('credits-visible');
+			toggleCredits(creditsToggleBtn, creditsSection, initialSection, isCreditsActive);
+		});
+
+		function toggleCredits(showTrigger, creditsSection, initialSection, isCreditsActive) {
+			if (isCreditsActive) {
+				showTrigger.removeClass('hidden');
+				showTrigger.data('credits-visible', false);
+				creditsSection.addClass("hidden");
+				window.setTimeout(function () {
+					initialSection.removeClass("hidden");
+				}, 300);
+			} else {
+				showTrigger.addClass('hidden');
+				showTrigger.data('credits-visible', true);
+				initialSection.addClass("hidden");
+				creditsSection.removeClass("hidden");
+			}
+
+		}
+	});
+})(jQuery);
 //////////////////////////////////////////////////////////
 /////////////////////// TESSERACT ////////////////////////
 //////////////////////////////////////////////////////////
