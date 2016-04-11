17516e8050c0cb11375de5e0f8306c11f9381f12
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2016-04-12 01:17:55 +0300

    added GA

--- js/app.js
+++ js/app.js
@@ -353,6 +353,15 @@ var FontDetector = function() {
 
     this.detect = detect;
 };
+// GOOGLE ANALYTICS GENERATED CODE
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-76281500-1', 'auto');
+		ga('send', 'pageview');
+// END GOOGLE ANALYTICS GENERATED CODE
 function drawTesseract(){
 
   var screen_canvas = document.getElementById('tesseract');
